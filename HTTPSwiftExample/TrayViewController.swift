b258636bd62c3dff73892d7b7972a7cbfce11c65
ChipH <cphchip@gmail.com>
2024-12-07 16:53:30 -0600

    Added functionality to navigate to a new tray view controller

---
+++ HTTPSwiftExample/TrayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrayViewController.swift
+//  HTTPSwiftExample
+//
+//  Created by Chip Henderson on 12/7/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class TrayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
