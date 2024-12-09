5251cc132cb08547dc4432e17b09c27c564c5f18
wacdav60 <wilmad@smu.edu>
2024-12-08 21:27:11 -0600

    Add Tray_History storyboard and seque from main
    Add Tray_History storyboard and seque from main

---
+++ HTTPSwiftExample/TrayHistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrayHistoryViewController.swift
+//  HTTPSwiftExample
+//
+//  Created by Wilma Davis on 12/8/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class TrayHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
